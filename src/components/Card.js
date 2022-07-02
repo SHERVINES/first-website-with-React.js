d6b7067accd98ed1ba1e29b8ef9492c997d2bd2b
shervin <shervin.ses@gmail.com>
2022-07-02 09:54:55 +0430

    add set to cards for buy number of phones

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./Card.module.css";
+import up from "../images/up.svg";
+import down from "../images/down.svg";
 
-const Card = (props) => {
-  const { image, name, price } = props;
-  return (
-    <div className={styles.container}>
-      <img src={image} alt="card" />
-      <h3>{name}</h3>
-      <p>{price}</p>
-    </div>
-  );
-};
+class Card extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  downHandler = () => {
+    if (this.state.counter >= 1) {
+      this.setState((prevState) => ({
+        counter: prevState.counter - 1,
+      }));
+    }
+  };
+  upHandler = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+  render() {
+    const { image, name, price } = this.props;
+    const { counter } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img src={image} alt="card" />
+        <h3>{name}</h3>
+        <p>{price}</p>
+        <div className={styles.counter}>
+          <img
+            className={this.state.counter ? "" : styles.deactive}
+            src={down}
+            alt="down-arrow"
+            onClick={this.downHandler}
+          />
+          <span>{counter}</span>
+          <img src={up} alt="up-arrow" onClick={this.upHandler} />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Card;
